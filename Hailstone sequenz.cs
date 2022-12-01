6584b15bd6ef4b990541f65b4ff355c5310ad550
Davos135 <113627854+Davos135@users.noreply.github.com>
2022-12-01 12:43:20 +0100

    Add files via upload

---
+++ Hailstone sequenz.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("start value");
+int n = int.Parse(Console.ReadLine()!);
+
+int x = 1;
+
+do
+{
+    x++;
+   
+    if (n % 2 == 0)
+    {
+        n = n / 2;
+
+    }
+    else { n = (3 * n) + 1; }
+   
+
+}
+
+while (n != 1);
+    System.Console.WriteLine($" the length of sequence  is {x}.");
+
+
