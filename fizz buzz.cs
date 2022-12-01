a0ef3aa13c1a56767afe0ea5bf65a387aa4406fb
Davos135 <113627854+Davos135@users.noreply.github.com>
2022-12-01 13:44:18 +0100

    Add files via upload

---
+++ fizz buzz.cs
@@ -0,0 +1,28 @@
+﻿
+
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+
+        
+
+    if (i % 15 == 0)
+    {
+        System.Console.WriteLine("fizzbuzz");
+    }
+          
+    else if (i % 5 == 0)
+
+    {
+        System.Console.WriteLine("buzz");
+        
+    }
+    else if (i % 3 == 0)
+    { System.Console.WriteLine("Fizz"); 
+    
+    } 
+    else
+  {System.Console.WriteLine(i);}
+  
+}
